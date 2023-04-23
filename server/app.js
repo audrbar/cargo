21e7b94efec6d35db69ac3f7093afa8c31a397a2
audrbar <audrbar@gmail.com>
2023-04-23 08:41:29 +0300

    server refact

--- server/app.js
+++ server/app.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const mysql = require('mysql');
-const fs = require('fs');
+
 const { v4: uuidv4 } = require('uuid');
 const md5 = require('md5');
-
+const { getGoodById, getGoods, deleteGood, createGood } = require('./goods');
+const { query, con } = require('./mysql');
 const app = express();
 const port = 3003;
-
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'db4'
-});
 
 app.use(cors({
     origin: 'http://localhost:3000',
@@ -32,51 +25,29 @@ app.use(express.json());
 
 // GOODS API GOODS API GOODS API GOODS API GOODS API GOODS API GOODS API
 
-app.get('/goods', (req, res) => {
-    const sql = `
-        SELECT id, title, weight, flammable, perishable
-        FROM goods
-    `;
-    con.query(sql, (err, result) => {
-        if (err) throw err;
+app.get('/goods', async (req, res) => {
+    try {
+        const result = await getGoods();
+        await deleteGood(result[0].id);
         res.json(result);
+    } catch (e) {
+        res.status(500);
+        res.json({ text: 'Failed to get items', type: 'error' });
+    }
+});
+
+app.post('/goods', async (req, res) => {
+    await createGood({ ...req.body });
+    res.json({
+        message: { text: 'New item was created.', 'type': 'success' }
     });
 });
 
-app.post('/goods', (req, res) => {
-    const sql = `
-        INSERT INTO goods ( title, weight, flammable, perishable )
-        VALUES (?, ?, ?, ?)
-    `;
-    con.query(sql, [req.body.title, req.body.weight, req.body.flammable, req.body.perishable], (err) => {
-        if (err) throw err;
-        res.json({
-            message: { text: 'New item was created.', 'type': 'success' }
-        });
+app.delete('/goods/:id', async (req, res) => {
+    await deleteGood(req.params.id);
+    res.json({
+        message: { text: 'New item was deleted.', 'type': 'success' }
     });
-});
-
-app.delete('/goods/:id', (req, res) => {
-
-    const sql = `
-        SELECT id, amount, blocked
-        FROM goods
-        WHERE id = ?
-    `;
-
-    con.query(sql, [req.params.id], (err, [account]) => {
-        if (err) throw err;
-        const sql = `
-        DELETE FROM goods
-        WHERE id = ?
-    `;
-        con.query(sql, [req.params.id], (err) => {
-            if (err) throw err;
-            res.json({
-                message: { text: 'The cargo item was deleted.', 'type': 'danger' }
-            });
-        });
-    })
 });
 
 
